76b4c62927793f1fc71d3c474990f626bd46f668
SharedOps <manikanta.mundru@gmail.com>
2019-01-14 22:33:08 +0200

    New webparts added in the solution

---
+++ src/webparts/fxThree/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IFxThreeWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'FxThreeWebPartStrings' {
+  const strings: IFxThreeWebPartStrings;
+  export = strings;
+}
---
+++ src/webparts/fxTwo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IFxTwoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'FxTwoWebPartStrings' {
+  const strings: IFxTwoWebPartStrings;
+  export = strings;
+}
